5de35842720a2e3ea618980e019483f5f33e44c2
Yesaaayy <127478697+Yesaaayy@users.noreply.github.com>
2024-12-01 15:11:52 +0800

    Add files via upload

---
+++ E-Blotter_System/src/main/java/com/mycompany/e/blotter_system/Dashboard.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.e.blotter_system;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author kylei
+ */
+public class Dashboard extends JFrame{
+    public void Login(){
+        setTitle("2nd Frame");
+        // Set the size of the frame before using it
+       setSize(1000, 700);
+        
+       setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+       setVisible(true);
+        
+    }
+}
---
+++ E-Blotter_System/src/main/java/com/mycompany/e/blotter_system/EBlotter_System.java
@@ -0,0 +1,10 @@
+package com.mycompany.e.blotter_system;
+
+
+class EBlotter_System {
+
+    public static void main(String[] args) {
+        Landing_Page fr = new Landing_Page();
+        fr.frames();
+    }
+}
